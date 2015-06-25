cff07ca3499b85033bf4c4288cf1b60b8e0cbfca
Thiago Hersan <thiago.hersan+github@gmail.com>
2015-06-25 00:45:21 -0500

    changes to examples/basic_receive.py

--- Python/examples/basic_receive.py
+++ Python/examples/basic_receive.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python
 
 import sc
 import time, threading
 import OSC
 
+FPS = 20.0
+LOOP_PERIOD = 1.0/FPS
+lastLoop = 0
 
-receive_address = 'localhost', 11112
-s = OSC.OSCServer(receive_address) # basic
+receive_address = '192.168.1.165', 1234
+s = OSC.OSCServer(receive_address)
 s.addDefaultHandlers()
 
 xb = 0
@@ -14,10 +18,10 @@ sb = 0
 xh = 0
 yh = 0
 sh = 0
-
+notasynth = False
 
 def blob_handler(addr, tags, stuff, source):
-    global xb, yb, sb
+    global xb, yb, sb, notasynth
     print "---"
     print "xb %s" % stuff[0]
     print "yb %s" % stuff[1]
@@ -26,7 +30,7 @@ def blob_handler(addr, tags, stuff, source):
     xb = stuff[0]
     yb = stuff[1]
     sb = stuff[2]
-
+    notasynth = True
 
 
 def haar_handler(addr, tags, stuff, source):
@@ -41,9 +45,8 @@ def haar_handler(addr, tags, stuff, source):
     sh = stuff[2]
 
 
-s.addMsgHandler("/b10s/blob",  blob_handler) # adding our function
-s.addMsgHandler("/b10s/haar", haar_handler) # adding our function
-
+s.addMsgHandler("/b10s/blob",  blob_handler)
+s.addMsgHandler("/b10s/haar", haar_handler)
 
 
 # just checking which handlers we have added
@@ -57,20 +60,24 @@ print "\nStarting OSCServer. Use ctrl-C to quit."
 st = threading.Thread( target = s.serve_forever )
 st.start()
 sc.start(spew=1)
-pulseTest = sc.Synth( "pulseTest" )
-
+# sine = sc.Synth( "sine" )
+Latch = sc.Synth ( "Latch")
 
 try :
     while True :
-        time.sleep(5)
-        pulseTest.maxPartial = xb
-        pulseTest.fund = yb
-        pulseTest.ampHz = sb
-
+        now = time.time()
+        if(now-lastLoop > LOOP_PERIOD):
+            lastLoop = now
+            Latch.rate = xb*20+1
+            # sine.freq = xb*600+120
+            #sine.amp = yb*2+1
+            time.sleep(LOOP_PERIOD/2)
 
 except KeyboardInterrupt :
     print "\nClosing OSCServer."
     s.close()
+    Latch.free()
+    sc.quit()
     print "Waiting for Server-thread to finish"
-    st.join() ##!!!
+    st.join()
     print "Done"
