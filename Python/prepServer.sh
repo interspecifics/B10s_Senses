e70617510482666026370fd3024b593ae5acd986
Thiago Hersan <thiago.hersan+github@gmail.com>
2015-06-28 01:27:26 -0500

    adds kills to prepServer script

--- Python/prepServer.sh
+++ Python/prepServer.sh
@@ -3,11 +3,17 @@
 # makes sure jack and scsynth are running
 uname=`whoami`
 
+# kills all python and sudo processes
+kpids=`ps -u root | grep 'ython\|sudo' | grep -v grep | awk '/ython|sudo/{print $1}'`
+sudo kill -9 $kpids
+
 isjack=`ps -u $uname | grep jack | grep -v grep | wc -l`
-if [ $isjack -lt 1 ]
+if [ $isjack -ge 1 ]
 then
-    jackd -p32 -dalsa -dhw:0,0 -p1024 -n3 -s &
+    killjackid=`ps -u $uname | awk '/jack/{print $1}'`
+    kill -9 $killjackid
 fi
+jackd -p32 -dalsa -dhw:0,0 -p1024 -n3 -s &
 
 issynth=`ps -u $uname | grep scsynth | grep -v grep | wc -l`
 if [ $issynth -ge 1 ]
